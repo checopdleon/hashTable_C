6c4eab77cde167b858897b6fb93011ff1ece9c0d
Checo Ponce De Leon <checopdleon@gmail.com>
2018-03-13 01:19:36 -0600

    Created hashedString() method.

--- hashTable.c
+++ hashTable.c
@@ -3,9 +3,11 @@
    Edits/Additions: n/a
    Course: Computer Architecture
    Project: #1 "Personnel Management System"
-   Last Edit: Mar. 1, 2018
+   Last Edit: Mar. 12, 2018
    !!!!!!!!!!  DO NOT DELETE  !!!!!!!!!!!
  */
+
+// !!!!!!!!!!!!!!!!!!  ADD '#ifndef' INCLUDE GUARD  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 #include<stdio.h>
 #include<string.h>
@@ -13,9 +15,13 @@
 #include"llist.h"
 #include"llist.c"
 
+/* First build the base struct that the LLists will be composed of, this
+ * struct is called LLItem and is within the 'llist.h' included file. Next
+ * is to create the struct of LLists, this is also in the 'llist.h' file.*/
+
 /** Array of lists, which make up the hashTable. Dynamically 
     resizing hashTable. */
-struct LList* hashTable;
+struct LList *hashTable;
 
 /** Atores the number of elements in the entire hash table. */
 int totalElements;
@@ -23,13 +29,26 @@ int totalElements;
 /** Max Llists the hashTable can store. */
 int maxLists = 31339;
 
-/** Hashes the given int passed in. */
-int hashedString (int key)  {
-  return key % maxLists;
+/** Return maxLists */
+int getMax() {
+  return maxLists;
+}
+
+/** Hashes the given int passed in to figure out which list corresponds
+    to the passed in string.
+    source: http://research.cs.vt.edu/AVresearch/hashing/strings.php*/
+int hashedString (char *string)  {
+  int i, sum = 0;
+  for (i = 0; i < strlen( string); i++)
+    {
+      sum += string[i];
+    }
+  
+  return sum % maxLists;
 }
 
 /** */
-struct node* get_elements(struct node *list, int find_index);
+struct node* get_elements(LList *list, int find_index);
 
 /** */
 void remove_element(int key);
@@ -60,5 +79,6 @@ int main() {
   hashTable = (struct LList*) malloc(31339 * sizeof(struct LList*) );
 
   printf("HashTable struct was created correctly\n");
+  
   return 0;
 }
